69cdb4d4f03931a50455d92bfeb3b14608b53e4e
jean andres <jeansi123@live.com>
2022-03-03 12:11:33 -0500

    subir taksApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
 
-function App() {
+
+interface iTask {
+name: string;
+done: boolean;
+
+}
+
+function App(): JSX.Element {
+
+    const [newTask,  setNewTask] = useState <string> ('');
+    const [task, setTask] = useState <iTask[]> ([]);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();  
+      addTask(newTask)
+      setNewTask('');
+      
+    }
+
+    const addTask = (name: string) => {
+
+     const newTask: iTask[] = [...task, {name, done:false}]
+     setTask(newTask);
+
+    }
+
+    const toggleDoneTask = (i: number) => {
+      const newTask: iTask[] = [...task];
+      newTask[i].done = !newTask[i].done;
+      setTask(newTask);
+    }
+
+    const removeTask = (i: number) => {
+
+      const newTask: iTask [] = [...task];
+      newTask.splice(i,1);
+      setTask(newTask);
+
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <div className="card">
+                  <form onSubmit={handleSubmit}>
+                    <input 
+                    className="form-control"
+                    type="text" 
+                    onChange={e => setNewTask(e.target.value)}
+                    value={newTask}
+                    autoFocus
+                  />
+                  <button className="btn btn-success btn-block mt-2">Save</button>
+                </form>
+              </div>
+            </div>
+          {
+            task.map((t: iTask, i: number)=> (
+              <div className="card card-body mt-2" key={i}>
+                <h2 style={{textDecoration: t.done ? 'line-through' : ''}}> {t.name} </h2>               
+
+                <div>
+                  <button className="btn btn-secondary" onClick={() => toggleDoneTask (i)}>
+                    {t.done ? '✓' : '✖'}
+                  </button>
+
+                  <button className="btn btn-danger" onClick={() => removeTask(i)}>
+                    🗑
+                  </button>
+                </div>
+              
+              
+              </div>
+
+              
+              
+            )
+              
+            )
+          }
+              </div>
+            </div>
+
+                  </div>
+
+                </div>
+      
+
+
+
+      
+
+
+    
   );
 }
 
